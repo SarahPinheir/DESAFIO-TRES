4f4ea495c72c20c2c4f3b0d5ff00b97ced46cd00
Sarah <sarahleticiasr@gmail.com>
2021-07-31 00:41:38 -0300

    Desafio Três Gama

---
+++ DotnetFrameworkDesafioTres/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetFrameworkDesafioTres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Para solucionar a seguinte expressão atribua valor a: x, y e d?"+"\n\n"+"Expressão: x + y + d + 5 * 77 + 8 - 10 * (8/3)"+"\n");
+            Console.WriteLine("Digite o valor de X");
+            double valorX = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor de Y");
+            double valorY = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor de D");
+            double valorD = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("O resultado da expressão para os valores informados é " + ((valorX + valorY + valorD + (5 * 77) + 8 - (10 * (8 / 3)))));
+            Console.ReadKey();
+
+
+        }
+    }
+}
